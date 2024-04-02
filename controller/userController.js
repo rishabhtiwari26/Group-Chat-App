f4d5280ce7d98c202fc5bae07de29bbed90b223a
= <rishabhtiwari26>
2024-04-02 15:04:04 +0530

    making backend for login

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,11 @@
 const User = require('../model/userModel')
 const bcrypt = require('bcrypt')
+const jwt =require('jsonwebtoken')
+function generateAccessToken(id){
+    return jwt.sign({userid:id},process.env.TOKEN_SECRET)
+
+
+}
 
 exports.signUp=(req,res,next)=>{
     const saltRounds=10
@@ -29,4 +35,32 @@ exports.signUp=(req,res,next)=>{
             })
     })
     
+}
+exports.login=(req,res,next)=>{
+    // console.log(req.body,req.body.id)
+    User.findOne({where:{email:req.body.email}})
+        .then(user=>{
+            try{if(user){
+                bcrypt.compare(req.body.password,user.password,(err,result)=>{
+                    if (err){
+                        throw new Error(err)
+                    }
+                    if(result===true){
+                        res.status(200).send({success:true,message:'User Login successfully',token:generateAccessToken(user.id)})
+                    }
+                    else{
+                        res.status(401).send({success:false,message:'Password do not match'})
+                    }
+                })
+                
+            }
+            else{ 
+                res.status(404).send({success:false,message:'User not found'})
+            }}catch{e=>{
+                console.log(e)
+            }
+
+            }
+            
+        })
 }
--- route/userRoute.js
+++ route/userRoute.js
@@ -3,6 +3,6 @@ const route=express.Router()
 const userController = require('../controller/userController')
 
 route.post('/signup',userController.signUp)
-
+route.post('/login',userController.login)
 
 module.exports=route
